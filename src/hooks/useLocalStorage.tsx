a8d2470efaf565ce6ece7e3353f435087349d4ce
Daniele Basile <daniele.basile.job@gmail.com>
2023-01-16 11:29:41 +0100

    all components added

---
+++ src/hooks/useLocalStorage.tsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
+  const [value, setValue] = useState<T>(() => {
+    const json = localStorage.getItem(key);
+    if (json !== null) return JSON.parse(json);
+    if (typeof initialValue === "function") {
+      return (initialValue as () => T)();
+    } else {
+      return initialValue;
+    }
+  });
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+  return [value, setValue] as [typeof value, typeof setValue];
+}
