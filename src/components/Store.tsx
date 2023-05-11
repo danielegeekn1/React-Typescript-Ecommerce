7fdade17d4d3fa0c6299e74330138b321c1ebe34
Daniele Basile <daniele.basile.job@gmail.com>
2023-05-11 17:02:42 +0200

    some style changes

--- src/components/Store.tsx
+++ src/components/Store.tsx
@@ -1,12 +1,28 @@
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/splide.min.css";
+import products from "../data/product.json";
 import StoreProducts from "./StoreProducts";
-
+import "./store.css";
 const Store = () => {
   return (
     <>
-      <StoreProducts />
-      {/* {products.map((item) => (
-        <StoreProducts {...item} />
-      ))} */}
+      <Splide
+        options={{
+          perPage: 3,
+          arrows: true,
+          pagination: false,
+          drag: "free",
+          gap: "3rem",
+        }}
+      >
+        {products.map((item) => (
+          <SplideSlide>
+            <div className="product">
+              <StoreProducts {...item} />
+            </div>
+          </SplideSlide>
+        ))}
+      </Splide>
     </>
   );
 };
--- src/components/StoreProducts.tsx
+++ src/components/StoreProducts.tsx
@@ -1,60 +1,45 @@
-import { Splide, SplideSlide } from "@splidejs/react-splide";
-import "@splidejs/splide/dist/css/splide.min.css";
-import "./storeProduct.css";
-import products from "../data/product.json";
 import { Button } from "react-bootstrap";
 import { useECommerceContext } from "../context/ShoppingCartContext";
-
+import "./storeProduct.css";
 type StoreProductProps = {
   id: number;
+  img: string;
+  title: string;
+  price: number;
 };
 
-const StoreProducts = ({ id }: StoreProductProps) => {
+const StoreProducts = ({ id, img, title, price }: StoreProductProps) => {
   const { addProduct, removeProduct, cartQuantity, getItemQuantity } =
     useECommerceContext();
   const quantity = getItemQuantity(id);
   return (
-    <div>
-      {cartQuantity}
-      <Splide
-        options={{
-          perPage: 4,
-          arrows: true,
-          pagination: false,
-          drag: "free",
-          gap: "3rem",
-        }}
-      >
-        {products.map((item, i) => (
-          <SplideSlide>
-            <div className="main_card">
-              <div key={i} className="card">
-                <img src={item.img} alt="product-images" />
+    <>
+      <div>{cartQuantity}</div>
+      <div className="main-card">
+        <div key={id} className="card">
+          <img src={img} alt="product-images" />
 
-                <h6>{item.price}</h6>
-                <h5>{item.title}</h5>
-                <div>
-                  {quantity > 1 && (
-                    <span className="text-muted" style={{ fontSize: ".65rem" }}>
-                      x{quantity}
-                    </span>
-                  )}
-                </div>
-              </div>
-              <div className="button-container">
-                <Button onClick={() => addProduct(item.id)}>
-                  Add product in the cart
-                </Button>
+          <h6>{price}</h6>
+          <h5>{title}</h5>
+          <div>
+            {quantity > 1 && (
+              <span className="text-muted" style={{ fontSize: ".65rem" }}>
+                x{quantity}
+              </span>
+            )}
+          </div>
+        </div>
+        <div className="button-container">
+          <Button onClick={() => addProduct(id)} className="btn">
+            Add product in the cart
+          </Button>
 
-                <Button onClick={() => removeProduct(item.id)}>
-                  Remove product from the cart
-                </Button>
-              </div>
-            </div>
-          </SplideSlide>
-        ))}
-      </Splide>
-    </div>
+          <Button onClick={() => removeProduct(id)} className="btn">
+            Remove product from the cart
+          </Button>
+        </div>
+      </div>
+    </>
   );
 };
 
